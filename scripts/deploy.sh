1ffd983a77a018911b6b868bea26ac62265462f5
fanzru <fattahilaaf080701@gmail.com>
2022-03-30 18:51:05 +0700

    bruh

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+cd ..
+echo "Deploying application ..."
+
+
+# Update codebase
+git fetch origin main
+git reset --hard origin/main
+
+echo "Installing dependencies 🛠"
+yarn install
+
+echo "Building application ⚙"
+yarn build
+            
+echo "Restart pm2 service 🔥"
+pm2 restart pm2.json
+
+echo "Application deployed!"
